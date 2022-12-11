c4c886e72512538b5758c5598de1cb2b4d122159
abey onalaja <abey.onalaja@gmail.com>
2022-12-11 23:22:15 +0000

    Add env config and docker setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+mod config;
 mod models;
 
 use crate::models::Status;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 use std::io;
 
 async fn status() -> impl Responder {
@@ -12,9 +14,17 @@ async fn status() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
     println!("Hello, world!");
+    println!(
+        "Starting server atr http://{}:{}/",
+        config.server.host, config.server.port
+    );
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind("127.0.0.1:8080")?
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
         .run()
         .await
 }
