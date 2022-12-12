bc69e150beac65ac2363e0b533d7a2ff90930906
abey onalaja <abey.onalaja@gmail.com>
2022-12-12 23:42:52 +0000

    create modeles for database tables

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ mod models;
 
 use crate::models::Status;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use deadpool_postgres::Runtime;
 use dotenv::dotenv;
 use std::io;
+use tokio_postgres::NoTls;
 
 async fn status() -> impl Responder {
     HttpResponse::Ok().json(Status {
@@ -18,13 +20,18 @@ async fn main() -> io::Result<()> {
 
     let config = crate::config::Config::from_env().unwrap();
 
+    let pool = config.pg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
     println!("Hello, world!");
     println!(
         "Starting server atr http://{}:{}/",
         config.server.host, config.server.port
     );
-    HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind(format!("{}:{}", config.server.host, config.server.port))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(pool.clone())
+            .route("/", web::get().to(status))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
 }
