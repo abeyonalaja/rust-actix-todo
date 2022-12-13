1b68c0cecbb591ab9fdbf2387115b689e88dca7f
abey onalaja <abey.onalaja@gmail.com>
2022-12-13 00:12:19 +0000

    connect todo list handler to route

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 mod config;
+mod db;
+mod handlers;
 mod models;
 
+use crate::handlers::*;
 use crate::models::Status;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use deadpool_postgres::Runtime;
 use dotenv::dotenv;
 use std::io;
 use tokio_postgres::NoTls;
-
-async fn status() -> impl Responder {
-    HttpResponse::Ok().json(Status {
-        status: "ok".to_string(),
-    })
-}
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -21,6 +19,7 @@ async fn main() -> io::Result<()> {
     let config = crate::config::Config::from_env().unwrap();
 
     let pool = config.pg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
+
     println!("Hello, world!");
     println!(
         "Starting server atr http://{}:{}/",
@@ -28,8 +27,9 @@ async fn main() -> io::Result<()> {
     );
     HttpServer::new(move || {
         App::new()
-            .app_data(pool.clone())
+            .app_data(Data::new(pool.clone()))
             .route("/", web::get().to(status))
+            .route("/todos", web::get().to(get_todo_list))
     })
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
